e3a55c1880e5d154d79576ed8a540ceb45edfaf3
PBS <sortsimilar@gmail.com>
2018-09-17 23:09:47 +0800

    question 37， page 10

---
+++ 03710.cpp
@@ -0,0 +1,110 @@
+#include<iostream>
+using namespace std;
+
+struct Node
+{
+	int key;
+	Node* next;
+};
+
+class List
+{
+	
+public:
+	Node * head, *tail; //tail help us add element at last;
+	List()
+	{
+		head = new Node;
+		head->next = NULL;
+		tail = head;
+	}
+
+	// print all node in list;
+	void print_list(Node *current)
+	{
+		
+		while(current!=NULL)
+		{
+			cout<<current->key<<" ";
+			current = current->next;
+		}
+	}
+
+	// insert a new node at last;
+	void insert_last(int value)
+	{
+		Node *temp = new Node;
+		temp->key = value;
+		temp->next = NULL;
+
+		tail->next = temp;
+		tail = temp;
+	}
+
+};
+
+
+
+int main()
+{
+	List test;
+	test.insert_last(10);
+	test.insert_last(20);
+	test.insert_last(30);
+	test.insert_last(40);
+	test.insert_last(50);
+	test.insert_last(60);
+
+
+//	test.print_list(test.head->next);
+
+	List link1;
+	List link2;
+
+	Node *current = test.head->next;
+	int counter = 0;
+
+	while(current != NULL)
+	{
+		if(counter%2 == 0)
+		{
+			link1.tail->next = current;
+			link1.tail = current;
+		}
+		else
+		{
+			link2.tail->next = current;
+			link2.tail = current;
+		}
+
+		current = current->next;
+		counter++;
+	}
+
+	link1.tail->next = NULL;
+	link2.tail->next = NULL;
+
+
+
+	current = link1.head->next;
+	while(current != NULL)
+	{
+		cout<<current->key<<" ";
+		current = current->next;
+	}
+
+	cout<<endl;
+
+	current = link2.head->next;
+	while(current != NULL)
+	{
+		cout<<current->key<<" ";
+		current = current->next;
+	}
+
+
+	return 0;
+}
+
+
+
